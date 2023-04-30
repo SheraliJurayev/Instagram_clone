01433902d53561916fd214052c7722ae5219898e
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-30 14:04:09 +0500

    Debug

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -9,10 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-od8-9&w-bs5qtjr=6vm&65jyccq*@)&7d_q9f0=mk9+*@3@t&&'
-SECRET_KEY = config('SECRET_KEY') 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
