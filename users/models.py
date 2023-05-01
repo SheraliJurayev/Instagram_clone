31d6d639e06d264ef8492c1b5c47eee3985fc848
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-05-01 11:45:34 +0500

    check_username , check_email, check_password

--- users/models.py
+++ users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractUser
 from shared.models import BaseModel
 
 from datetime import datetime , timedelta
+import random
+import uuid
 
 
 
@@ -34,13 +36,46 @@ class User(AbstractUser , BaseModel):
     email = models.EmailField(null=True , unique=True,blank=True)
     phone_number = models.CharField(max_length=13 , null=True , unique=True ,blank=True)
     photo = models.ImageField(upload_to='user_photos/', null=True ,blank=True , 
-                         validators=FileExtensionValidator(allowed_extensions=['jpg', 'jpeg' ,'png']) , unique=True , blank=True
+                         validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg' ,'png' , 'heic' , 'heif'])]
                                )
 
 
     def __str__(self):
         return self.username
     
+    @property
+    def full_name(self):
+        return f'{self.first_name}{self.last_name}'
+    
+    def create_verify_code(self, verify_type):
+        code = "".join([str(random.randint(0,100)%10)for _ in range(4)])
+        UserConfirmation.objects.create(
+            user_id=self.id,
+            verify_type=verify_type,
+            code=code
+        ) 
+
+        return code
+    
+    def check_username(self):
+        if not self.username:
+            temp_username = f'instagram-{uuid.uuid4().__str__().split("-")[-1]}'
+            while User.objects.filter(username = temp_username):
+                temp_username = f'{temp_username}{random.randint(0 , 9)}'
+            self.username = temp_username
+
+
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower()
+            self.email = normalize_email
+
+    def check_password(self):
+        if not self.password:
+            temp_password = f'password-{uuid.uuid4().__str__().split("-")[-1]}'     
+            self.password = temp_password
+            
+                           
 PHONE_EXPIRE = 2
 EMAIL_EXPAIR = 5
 
