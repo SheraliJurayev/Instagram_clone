afed71efecc063e753e4f1d47a4f7738908b0803
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-05-02 18:00:13 +0500

    users:  serializers , urls

---
+++ users/serializers.py
@@ -0,0 +1,31 @@
+from .models import User , UserConfirmation , VIA_EMAIL , VIA_PHONE , NEW , CODE_VERIFIED , DONE , PHOTO_STEP
+from rest_framework import exceptions , serializers
+from django.db.models import Q
+from rest_framework.exceptions import ValidationError
+
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpSerializer , self).__init__(*args, **kwargs)
+        self.fields['email_phone_number'] = serializers.CharField(required = False)
+
+    class Meta:
+        model = User
+        fields = (
+            'id', 
+            'auth_type',
+            'auth_status'
+        )
+
+        extra_kwargs = {
+            'auth_type': { 'read_only':True , 'required' : False} , 
+            'auth_status': { 'read_only':True , 'required' : False} , 
+
+        }
+
+    @staticmethod
+    def auth_validate(attrs):
+        pass
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from .models import User
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny)
+
