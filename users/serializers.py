17e264c5c70fd179ea027fc1342be92fe5c56b45
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-05-14 00:44:24 +0500

    utility , serializer

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,7 @@ from .models import User , UserConfirmation , VIA_EMAIL , VIA_PHONE , NEW , CODE
 from rest_framework import exceptions , serializers
 from django.db.models import Q
 from rest_framework.exceptions import ValidationError
+from shared.utility import check_email_or_phone
 
 
 
@@ -25,6 +26,20 @@ class SignUpSerializer(serializers.ModelSerializer):
             'auth_status': { 'read_only':True , 'required' : False} 
         }
 
+    def create(self, validated_data):
+        user  = super(SignUpSerializer , self).create(validated_data)
+        print(user)
+        if user.auth_type == VIA_EMAIL:
+            code = user.create_verify_code(VIA_EMAIL)
+            print(code)
+            # send_mail(user.mail , code)
+
+        elif  user.auth_type == VIA_PHONE:
+            code  = user.create_verify_code(VIA_PHONE)
+            # send_phone_code(user.phone_number, code)
+
+        user.save()    
+        return user
     def validate(self, data):  
         super(SignUpSerializer , self).validate(data)
         data =self.auth_validate(data)  
@@ -33,5 +48,31 @@ class SignUpSerializer(serializers.ModelSerializer):
     @staticmethod
     def auth_validate(data):
         print(data)
-        user_input = str(data.get('email_phone_number')).lower
+        user_input = str(data.get('email_phone_number')).lower()
+        input_type = check_email_or_phone(user_input)
+        if input_type == 'email':
+            data = {
+                'email' : user_input ,
+                'auth_type' : VIA_EMAIL
+            }
+        elif input_type == 'phone':
+             data = {
+                'phone_number' : user_input ,
+                'auth_type' : VIA_PHONE
+            }
+             
+        else:
+            data = {
+                'success' : False , 
+                'message' : 'You must send email or phone number'
+            }
+            raise ValidationError(data)
         return data
+    
+    def validate_email_phone_number(self , value):
+        value = value.lower()
+
+        return value
+
+
+         
